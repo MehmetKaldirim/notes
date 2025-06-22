f10e2ea4106bd8fe98502be16360d7ca6cf2d208
Mehmet <mathdenizi@gmail.com>
2025-06-22 23:23:16 +0200

    adding default spring filter with one use password and default user

---
+++ src/main/java/com/mathdenizi/notes/HelloController.java
@@ -0,0 +1,17 @@
+package com.mathdenizi.notes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mathdenizi
+ * Date: 21.06.25
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
