4a6caaa4f4f61cd2487ccf3c4f64b734eeef1b90
Mehmet <mathdenizi@gmail.com>
2025-06-30 00:54:15 +0200

    method level security

--- src/main/java/com/mathdenizi/notes/controllers/AdminController.java
+++ src/main/java/com/mathdenizi/notes/controllers/AdminController.java
@@ -6,6 +6,7 @@ import com.mathdenizi.notes.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,12 +22,14 @@ public class AdminController {
     @Autowired
     UserService userService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getusers")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(),
                 HttpStatus.OK);
     }
 
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/update-role")
     public ResponseEntity<String> updateUserRole(@RequestParam Long userId,
                                                  @RequestParam String roleName) {
@@ -34,6 +37,7 @@ public class AdminController {
         return ResponseEntity.ok("User role updated");
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
         return new ResponseEntity<>(userService.getUserById(id),
