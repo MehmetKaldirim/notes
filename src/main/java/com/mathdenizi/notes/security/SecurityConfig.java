389bb16ecc8e5ac667a682b6d7c56bbed6246fe7
Mehmet <mathdenizi@gmail.com>
2025-07-11 03:06:33 +0200

    csrf protection added to project

--- src/main/java/com/mathdenizi/notes/security/SecurityConfig.java
+++ src/main/java/com/mathdenizi/notes/security/SecurityConfig.java
@@ -17,9 +17,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 import java.time.LocalDate;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created by mathdenizi
@@ -31,21 +34,20 @@ import java.time.LocalDate;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, CustomLoggingFilter customLoggingFilter) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            (requests
-                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                    .anyRequest()).authenticated();
-        });
-
-        http.csrf(AbstractHttpConfigurer::disable);
-        /*http.addFilterBefore(new CustomLoggingFilter(),
-                UsernamePasswordAuthenticationFilter.class);
-        http.addFilterAfter(new RequestValidationFilter(),
-                CustomLoggingFilter.class);*/
-        //http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("/api/auth/public/**")
+        );
+        //http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((requests)
+                -> requests
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/csrf-token").permitAll()
+                .anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
     }
 
 
