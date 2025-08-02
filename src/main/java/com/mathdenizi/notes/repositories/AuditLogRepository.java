a30bad0169a4771b3343def7737d1304fd806e69
Mehmet <mathdenizi@gmail.com>
2025-08-02 03:11:03 +0200

    auditing controller created

--- src/main/java/com/mathdenizi/notes/repositories/AuditLogRepository.java
+++ src/main/java/com/mathdenizi/notes/repositories/AuditLogRepository.java
@@ -3,5 +3,8 @@ package com.mathdenizi.notes.repositories;
 import com.mathdenizi.notes.models.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByNoteId(Long noteId);
 }
